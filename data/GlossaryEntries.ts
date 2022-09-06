4f0ba982cc5ceaed992db15bb8af730a930f184a
Larry Golding <larrygolding@comcast.net>
2022-09-06 15:06:40 -0700

    Extract data from Glossary page and import it.

---
+++ data/GlossaryEntries.ts
@@ -0,0 +1,22 @@
+import GlossaryEntry from "../pages/models/GlossaryEntry";
+
+const data: GlossaryEntry[] = [
+  {
+    term: "Agile",
+    definition:
+      "A set of software development principles and practices designed to allow teams to respond quickly to changes in requirements and the business environment.",
+  },
+  {
+    term: "microservice",
+    definition:
+      "One of a set of program components that are “highly maintainable and testable, loosely coupled, independently deployable, organized around business capabilities, [and] owned by a small team.”",
+    source: "https://microservices.io/",
+  },
+  {
+    term: "patching",
+    definition:
+      "The process of updating the operating system or applications installed on a computer to mitigate security vulnerabilities or to fix bugs.",
+  },
+];
+
+export default data;
--- pages/terms/index.tsx
+++ pages/terms/index.tsx
@@ -1,24 +1,6 @@
 import { useEffect, useState } from "react";
 import GlossaryEntry from "../models/GlossaryEntry";
-
-const data: GlossaryEntry[] = [
-  {
-    term: "Agile",
-    definition:
-      "A set of software development principles and practices designed to allow teams to respond quickly to changes in requirements and the business environment.",
-  },
-  {
-    term: "microservice",
-    definition:
-      "One of a set of program components that are “highly maintainable and testable, loosely coupled, independently deployable, organized around business capabilities, [and] owned by a small team.”",
-    source: "https://microservices.io/",
-  },
-  {
-    term: "patching",
-    definition:
-      "The process of updating the operating system or applications installed on a computer to mitigate security vulnerabilities or to fix bugs.",
-  },
-];
+import data from "../../data/GlossaryEntries";
 
 function Glossary() {
   const [terms, setTerms] = useState<GlossaryEntry[]>([]);
