a3792a574477134bd0f07cb788a7f18f2bda8c31
Larry Golding <larrygolding@comcast.net>
2023-01-01 09:19:47 -0800

    Import test data from data/GlossaryEntries.

--- __tests__/pages/terms/index.test.tsx
+++ __tests__/pages/terms/index.test.tsx
@@ -1,26 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Glossary from "../../../pages/terms/index";
-import GlossaryEntry from "../../../models/GlossaryEntry";
-
-const testData: GlossaryEntry[] = [
-  {
-    term: "Agile",
-    definition:
-      "A set of software development principles and practices designed to allow teams to respond quickly to changes in requirements and the business environment.",
-  },
-  {
-    term: "microservice",
-    definition:
-      "One of a set of program components that are “highly maintainable and testable, loosely coupled, independently deployable, organized around business capabilities, [and] owned by a small team.”",
-    source: "https://microservices.io/",
-  },
-  {
-    term: "patching",
-    definition:
-      "The process of updating the operating system or applications installed on a computer to mitigate security vulnerabilities or to fix bugs.",
-  },
-];
+import testData from "../../../data/GlossaryEntries";
 
 describe("The Glossary page", () => {
   beforeEach(() => {
