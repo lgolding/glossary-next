1c3512d7cdcb54e180bdad5494bd5545607b71c1
Larry Golding <larrygolding@comcast.net>
2023-01-02 10:58:40 -0800

    Use custom document for common Head contents.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,19 +2,7 @@ import Head from "next/head";
 import { FC, Fragment, ReactNode } from "react";
 
 const Layout: FC<{ children: ReactNode }> = ({ children }) => {
-  return (
-    <Fragment>
-      <Head>
-        <title>Glossary Application</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
-      </Head>
-      {children}
-    </Fragment>
-  );
+  return <Fragment>{children}</Fragment>;
 };
 
 export default Layout;
